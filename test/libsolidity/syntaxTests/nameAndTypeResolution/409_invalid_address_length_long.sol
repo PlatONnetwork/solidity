617ce7574afd1cca58148438eef749e01b1e4367
alliswell <151259511@qq.com>
2020-05-09 08:28:40 +0800

    update sol cases

--- test/libsolidity/syntaxTests/nameAndTypeResolution/409_invalid_address_length_long.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/409_invalid_address_length_long.sol
@@ -1,8 +1,8 @@
 contract C {
     function f() pure public {
-        address x = 0xFA0bFc97E48458494Ccd857e1A85DC91F7F0046E0;
+        address x = 'lat15zlujljggkzffnxc2ls6shwfralsq3hq00psx';
         x;
     }
 }
 // ----
-// TypeError: (54-109): Type int_const 2284...(42 digits omitted)...9360 is not implicitly convertible to expected type address.
+// TypeError: (52-107): Type literal_string "lat15zlujljggkzffnxc2ls6shwfralsq3hq00psx" is not implicitly convertible to expected type address.
